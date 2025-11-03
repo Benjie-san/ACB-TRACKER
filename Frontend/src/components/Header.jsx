5946ee3dd354e18e00e86d78edc860f07f0382d1
Benjie-San <haisesasaki56@gmail.com>
2025-11-03 19:52:27 +0800

    added logo

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { ReactComponent as AccessLogo } from '../assets/ACB-BLUE.svg';
+import AccessLogo from '../assets/ACB-BLUE.svg';
 function Header() {
     return (
         <div style={styles.container}>
 
             <h1>ACCESS</h1>
-            <AccessLogo style={{ height: 50, width: 50 }} />
+            <img src={AccessLogo} alt="Access Customs Brokerage Logo" style={{ height: 50, width: 50 }} />
         </div>
     )
 }
-r
+
 const styles = {
     container: {
         color: 'red',
